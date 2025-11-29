4dd085ede563def8ff80e095a61585f3deea10f3
Valdemir <valdemirpraxedes92@gmail.com>
2025-11-29 18:17:12 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Às vezes, uma única escolha muda tudo. Eu decidi mudar — e você vai perceber isso muito em breve.</h1>
+  return (
+    <h1>
+      Às vezes, uma única escolha muda tudo. Eu decidi mudar — e você vai
+      perceber isso muito em breve.
+    </h1>
+  );
 }
 
 export default Home;
